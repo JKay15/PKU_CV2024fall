7cbad4a76955fcd71c3c38e8e2dea65719ff4dc8
Str8Chr <1924896401@qq.com>
2024-12-22 19:07:51 +0800

    Refine

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,6 @@
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 8
 gatys.py,6\5\65c989393cff781dc5c28a9c8f9e751c2e4bfac2
+:
+
+test.ipynb,9\0\90479d8bc9f8dfa7116261e4ed6ed71a98437648
--- gatys.py
+++ gatys.py
@@ -11,58 +11,45 @@ from tqdm import tqdm
 import argparse
 
 
-class VGGNet(nn.Module):
+# https://pytorch.org/tutorials/advanced/neural_style_tutorial.html
+# https://github.com/leongatys/PytorchNeuralStyleTransfer/blob/master/NeuralStyleTransfer.ipynb
+
+
+class FeatureExtractor(nn.Module):
     def __init__(self):
         super().__init__()
         # content on conv4_2
         # style on conv1_1, conv2_1, conv3_1, conv4_1, conv5_1
-        self.select = ['0', '5', '10', '19', '28', '20']
-        self.vgg = models.vgg19(weights=True).features  # pretrained VGG19 model
+        self.select = [0, 5, 10, 19, 28, 21]
+        self.vgg = models.vgg19(weights=True).features[:29]  # pretrained
         for param in self.vgg.parameters():
             param.requires_grad_(False)
+        for i, layer in enumerate(self.vgg):
+            if isinstance(layer, nn.MaxPool2d):
+                self.vgg[i] = nn.AvgPool2d(kernel_size=2, stride=2)
 
     def forward(self, x):
         features = []
-        for name, layer in self.vgg._modules.items():
+        for i, layer in enumerate(self.vgg):
             x = layer(x)
-            if name in self.select:
+            if i in self.select:
                 features.append(x)
         return features
 
 
-def preprocess(
+def image_loader(
         image_path,
-        max_size=None,
-        shape=None,
         transform=None,
-        device=None
+        device=torch.device("cpu"),
 ):
     """
     read an image
     process it to tensor available for vgg
     """
     image = Image.open(image_path)
-    if max_size:
-        scale = max_size / max(image.size)
-        size = np.array(image.size) * scale
-        image = image.resize(size.astype(int), Image.ANTIALIAS)
-    if shape:
-        image = image.resize(shape, Image.LANCZOS)
     if transform:
         image = transform(image).unsqueeze(0)
-    return image.to(device)
-
-
-def postprocess(tensor):
-    """
-    convert a tensor to an image
-    """
-    image = tensor.to("cpu").clone().detach()
-    image = image.numpy().squeeze()
-    image = image.transpose(1, 2, 0)
-    image = image * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
-    image = image.clip(0, 1)
-    return image
+    return image.to(device, torch.float)
 
 
 def get_features(image, model):
@@ -78,89 +65,103 @@ def get_features(image, model):
 
 
 def gram_matrix(tensor):
+    c, hw = tensor.size()
     gram = torch.mm(tensor, tensor.t())
-    return gram
+    return gram / hw
 
 
 if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
 
-    image_size = 356
+    image_size = 512 if device == torch.device("cuda") else 256
     img_path = './img/'
     save_path = './results/'
-    content_path = f'{img_path}content_palace.jpg'
-    style_path = f'{img_path}style_vanGogh.jpg'
+    # content_name = 'Tuebingen_Neckarfront'
+    # style_name = 'vangogh_starry_night'
+    content_name = 'content_palace'
+    style_name = 'style_vanGogh'
+    content_path = f'{img_path}{content_name}.jpg'
+    style_path = f'{img_path}{style_name}.jpg'
 
     transform = transforms.Compose([
+        transforms.Resize(image_size),
         transforms.ToTensor(),
         transforms.Normalize(
             (0.485, 0.456, 0.406),
-            (0.229, 0.224, 0.225)
-        )
+            (1, 1, 1),
+        ),
+        transforms.Lambda(lambda x: x[torch.LongTensor([2, 1, 0])]),
+        transforms.Lambda(lambda x: x.mul(255)),
+    ])
+
+    post_process = transforms.Compose([
+        transforms.Lambda(lambda x: x.mul(1/255)),
+        transforms.Lambda(lambda x: x[torch.LongTensor([2, 1, 0])]),
+        transforms.Normalize(
+            (-0.485, -0.456, -0.406),
+            (1, 1, 1),
+        ),
+        transforms.Lambda(lambda x: x.clamp(0, 1)),
+        transforms.ToPILImage(),
     ])
 
     # load content and style image
-    content = preprocess(
+    content = image_loader(
         content_path,
-        shape=[image_size, image_size],
         transform=transform,
         device=device
     )
-    style = preprocess(
+    style = image_loader(
         style_path,
-        shape=[image_size, image_size],
         transform=transform,
         device=device
     )
 
-    model = VGGNet().to(device).eval()
+    model = FeatureExtractor().to(device).eval()
 
     # get content and style features
-    content_features = get_features(content, model)
-    style_features = get_features(style, model)
-    style_grams = [gram_matrix(feature[:-1]) for feature in style_features]
+    content_feature = get_features(content, model)[-1]
+    style_features = get_features(style, model)[:-1]
+    style_grams = [gram_matrix(feature) for feature in style_features]
 
-    # white noise image
-    target = torch.randn_like(content).requires_grad_(True).to(device)
+    # # white noise image
+    # target = torch.randn_like(content).requires_grad_(True).to(device)
+    target = content.clone().requires_grad_(True)
 
-    w = 1/5  # w_l
-    alpha = 1  # alpha
-    beta = 1e5  # beta
-    content_weight = 1 / 2
-    style_weights = [1 / (f.shape[0]**2 * f.shape[1]**2) / 4 for f in style_features]
+    content_weight = 1
+    style_weights = [1e3 / n**2 for n in [64, 128, 256, 512, 512]]
     optimizer = optim.LBFGS([target])
-    steps = 500
+    steps = 100
     show_every = 10
 
     def closure():
         optimizer.zero_grad()
         target_features = get_features(target, model)
-        content_loss = F.mse_loss(target_features[-1], content_features[-1], reduction='sum') * content_weight
+        target_content = target_features[-1]
+        target_styles = target_features[:-1]
+        content_loss = F.mse_loss(target_content, content_feature) * content_weight
         style_loss = 0
-        for target_feature, style_gram, weight in zip(
-                target_features,
+        for target_style, style_gram, style_weight in zip(
+                target_styles,
                 style_grams,
-                style_weights
+                style_weights,
         ):
-            target_gram = gram_matrix(target_feature[:-1])
-            style_loss += F.mse_loss(target_gram, style_gram, reduction='sum') * weight * w
-        total_loss = alpha * content_loss + beta * style_loss
+            target_gram = gram_matrix(target_style)
+            style_loss += F.mse_loss(target_gram, style_gram) * style_weight
+        total_loss = content_loss + style_loss
         total_loss.backward()
         return total_loss
 
     for i in tqdm(range(1, steps + 1)):
         optimizer.step(closure)
-        loss = closure()
         if i % show_every == 0:
-            plt.imshow(postprocess(target))
-            plt.axis("off")
+            loss = closure()
+            plt.imshow(post_process(target[0].cpu().detach()))
             plt.title(f"Iter {i}, Loss: {loss.item()}")
             plt.show()
 
     # save image
-    plt.imshow(postprocess(target))
-    plt.axis("off")
-    content_name = content_path.split('/')[-1].split('.')[0]
-    style_name = style_path.split('/')[-1].split('.')[0]
-    plt.savefig(f'{save_path}{content_name}_{style_name}.png')
-    plt.show()
+    result = post_process(target[0].cpu().detach())
+    result = Image.fromarray((result * 255).astype(np.uint8))
+    result.save(f'{save_path}{content_name}_{style_name}.jpg')
